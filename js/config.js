b86058df2728833f8819eb59334645ebe5581ec7
oliver-wahome <olivernderitu135@gmail.com>
2022-12-15 10:04:42 +0300

    update: adding javascript files to project

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATeEp1WGStP7mW76T8NLPnU6qaERHpSis",
+    authDomain: "officemgtsystem-6af48.firebaseapp.com",
+    projectId: "officemgtsystem-6af48",
+    storageBucket: "officemgtsystem-6af48.appspot.com",
+    messagingSenderId: "263073508339",
+    appId: "1:263073508339:web:cbbb33ed7ac47a13848086",
+    measurementId: "G-E6PYZPDBSJ"
+};
+
+firebase.initializeApp(firebaseConfig);
