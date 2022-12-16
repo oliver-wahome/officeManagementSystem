a8e3d8b098a8ee3840f8d0db1d1ac745c8edcf45
oliver-wahome <olivernderitu135@gmail.com>
2022-12-16 09:13:19 +0300

    update: income modal added onclick of Add Income btn

--- js/main.js
+++ js/main.js
@@ -8,9 +8,27 @@ function registerUser(){
     
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userInfo) => {
-            console.log("User Logged in");
+            console.log("User registered and logged in");
+            window.location.href = "dashboard.html";
         })
         .catch((error) => {
             console.log(error.Message);
+        })
+}
+
+//checking if user has a firebase authentication account
+function loginUser() {
+    var email = document.getElementById("loginEmail").value;
+    var password = document.getElementById("loginPassword").value;
+
+    console.log(email + " " + password);
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userInfo) => {
+            console.log("User logged In");
+            window.location.href = "dashboard.html";
+        })
+        .catch((error) => {
+            console.log(error.message);
         })
 }
